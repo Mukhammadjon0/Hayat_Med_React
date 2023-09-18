0568dd36134117fe1c3920c2a50b07ce2bff2087
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-09-18 18:27:57 +0500

    connect bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
