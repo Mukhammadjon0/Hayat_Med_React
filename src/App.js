7a66f2fdb36f805d40db5f2da06d2c3a2a25b95d
Mukhammadjon0 <zokirjonovmuhammadjon0@gmail.com>
2023-09-22 21:09:58 +0500

    login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
